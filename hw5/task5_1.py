44628d3d003036a7d44c6782f70b693b0087a03a
Alexandra <gosteli.alexandra@gmail.com>
2023-03-24 21:02:54 +0100

    task 5_1 done

---
+++ hw5/task5_1.py
@@ -0,0 +1,17 @@
+# 5.1[26]: Напишите рекурсивную функцию для возведения числа a в степень b. 
+# Разрешается использовать только операцию умножения. 
+# Циклы использовать нельзя
+# Примеры/Тесты:
+# <function_name>(2,0) -> 1
+# <function_name>(2,1) -> 2
+# <function_name>(2,3) -> 8
+# <function_name>(2,4) -> 16
+
+def mult_power(n , power):
+   if power == 1:
+       return n
+    
+   if power != 1:
+       return n * mult_power(n, power - 1)
+
+print(mult_power(2,4))
